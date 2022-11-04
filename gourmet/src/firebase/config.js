2b71535bc4635eb90a6e540c185f6fa40c3dd954
idgidikov <idgidikov@abv.bg>
2022-11-04 15:00:19 +0200

    firebase Config

---
+++ gourmet/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsu2lhYQ2dBPyPE6SSqodR3BdFwc1tiYo",
+  authDomain: "gourmet-7c0a4.firebaseapp.com",
+  projectId: "gourmet-7c0a4",
+  storageBucket: "gourmet-7c0a4.appspot.com",
+  messagingSenderId: "1042249482251",
+  appId: "1:1042249482251:web:4efb5ea99e7c9e26b1870e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
