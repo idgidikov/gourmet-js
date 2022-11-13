af8de9c22025cfdc40f5fdcce053b84668c18a9a
idgidikov <idgidikov@abv.bg>
2022-11-13 16:20:23 +0200

    merge fixed

--- gourmet/src/components/forum/MenuBlog.jsx
+++ gourmet/src/components/forum/MenuBlog.jsx
@@ -15,32 +15,20 @@ const MenuBlog = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-<<<<<<< HEAD
-                    <a className="btn">Last added</a>
-                    <a className="btn">Most Liked</a>
-                    <a className="btn">Most Comment</a>
-=======
                         <Link to='/blog-posts/last-added' className="btn">lastAdded</Link>
                         <Link to='/blog-posts/most-commented' className="btn">Most Commented</Link>
                         <Link to='/blog-posts/most-liked' className="btn">Most Liked</Link>
                       
 
 
->>>>>>> b613f412296b8b500ffe9ad4fb41c7c72a4ee22c
                     </ul>
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-<<<<<<< HEAD
-                    <a className="btn">Last added</a>
-                    <a className="btn ml-3">Most Liked</a>
-                    <a className="btn ml-3">Most Comment</a>
-=======
                     <Link to='/blog-posts/last-added' className="btn">Last Added</Link>
                     <Link to='/blog-posts/most-commented' className="btn">Most Commented</Link>
                     <Link to='/blog-posts/most-liked' className="btn">Most Liked</Link>
->>>>>>> b613f412296b8b500ffe9ad4fb41c7c72a4ee22c
                 </ul>
             </div>
             <div className="navbar-end">
