62ec6bbdd330c1a9d2a493e55d4b4e669105d0db
idgidikov <idgidikov@abv.bg>
2022-11-14 16:26:14 +0200

    pagination on Allpost added

--- gourmet/src/services/post.services.js
+++ gourmet/src/services/post.services.js
@@ -1,4 +1,4 @@
-import { ref, push, get, set, update, remove, query, equalTo, orderByChild, orderByKey } from 'firebase/database'
+import { ref, push, get, set, update, remove, query, equalTo, orderByChild, orderByKey,limitToFirst,startAt } from 'firebase/database'
 import { db, storage } from '../firebase/config'
 
 
@@ -41,6 +41,20 @@ export const getAllPosts = async () => {
     return Object
       .keys(snapshot.val())
       .map(key => ({...snapshot.val()[key], id: key}))
+}
+export const getPostsPage = async (pageSize=3,page=0) => {
+  const posts = await get(ref(db, 'posts'))
+  const postKeys = Object.keys(posts.val())
+  //console.log(postKeys)
+  const start = page*pageSize
+  const end = start+pageSize
+  const pageKeys = postKeys.slice(start,end)
+  
+  if (!posts.exists()) {
+    return []
+  }
+
+  return pageKeys.map(key => ({...posts.val()[key], id: key}))
 }
 
 export const getPostById = async (id) => {
--- gourmet/src/views/forum/AllPost.jsx
+++ gourmet/src/views/forum/AllPost.jsx
@@ -6,16 +6,19 @@ import { useEffect } from 'react'
 import { useContext } from 'react'
 import { AppContext } from '../../context/app.context'
 import PostsCard from '../../components/forum/PostCard'
+import { getPostsPage } from "../../services/post.services"
 
 const AllPosts = () => {
     const [posts, setPosts] = useState([])
     const { addToast } = useContext(AppContext)
+    const [page, setPage] = useState(0)
 
     useEffect(() => {
-        getAllPosts().then((res) => {
+        const pageSize = 3
+        getPostsPage(pageSize,page).then((res) => {
             setPosts(res)
         }).catch(e => addToast('error', e.message))
-    }, [])
+    }, [page])
     
     return (
         <div className="AllPost">
@@ -25,6 +28,8 @@ const AllPosts = () => {
             <div className="flex flex-wrap justify-around">
                 {posts.map(p => <PostsCard key={p.id} post={p} />)}
             </div>
+            <button className="btn btn-primary" disabled={page === 0} onClick={() => setPage(page - 1)}>Prev</button>
+        <button className="btn btn-primary" onClick={() => setPage(page + 1)}>Next</button>
             
         </div>
         )
